16b161c194d9f5d16a06f155844b8fda06fbaefc
JavaAndCpp <83649002+JavaAndCpp@users.noreply.github.com>
2021-05-12 00:25:44 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-from kakaopy.client import client
+from kakaopy.client import Client
 import json
 
-class 
+class main(Client):
+  async def onMessage(self, chat):
+    print(chat.message)
+    
+    if chat.message == "power":
+      await chat.reply("kakaopy is runnnnnnnnning~")
+      
+    if chat.message == "Hello":
+      attachment = {'mentions': [{'user_id': chat.authorId, 'at': [1], 'len': 2}]}
+      await chat.channel.sendChat("Hello~ " + "@태그",json.dumps(attachment),1) 
+      
+      #If you have open chat permission 
+      if chat.message == ".hide":
+        await.chat.hide()
+        
